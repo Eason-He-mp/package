61c36414ac8524f59ee626dfce9975afc1e3b92c
Eason-He-mp <hyc990204@gmail.com>
2026-07-15 15:37:02 +0800

    Update 2D_scan.py

--- 2D_scan.py
+++ 2D_scan.py
@@ -476,7 +476,7 @@ def stitch_images():
 
         # ---------- 生成 ImageJ 宏（针对临时目录）----------
         safe_dir = temp_dir.replace('\\', '/')
-        file_template = f"{prefix}_%d{ext}"   # 连续编号，无前导零
+        file_template = f"{prefix}{{iii}}{ext}"   # 连续编号，无前导零
 
         # 注意：以下参数中特意保留了多余空格，请勿删除（防止 Notepad 换行解析错误）
         macro_args = (
